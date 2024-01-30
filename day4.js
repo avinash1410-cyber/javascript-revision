d5753c14186122980132d88242221070a4f2c46c
avinash.kumar1 <avinash.kumar1@spinny.com>
2024-01-30 17:02:51 +0530

    opeartion and comparision

---
+++ day4.js
@@ -0,0 +1,23 @@
+//comparision
+// console.log("2">1) //true
+// console.log("02">3)  //false
+
+// console.log(null >0)  //false
+// console.log(null==0) //false
+// console.log(null>=0) // true bcz the comaprision convrt the null to 0
+
+
+
+// console.log(undefined >0)  //false
+// console.log(undefined==0) //false
+// console.log(undefined>=0) // false
+
+
+
+//strict check ===
+
+console.log("2"==2) //checking only value 
+console.log("2"===2) // also check the dataypes value 
+
+
+
