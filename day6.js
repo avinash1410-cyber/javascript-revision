34323ed4281f28e7aeca216bd044d7a042879e0b
avinash.kumar1 <avinash.kumar1@spinny.com>
2024-01-30 21:47:58 +0530

    memory

---
+++ day6.js
@@ -0,0 +1,30 @@
+//Memory
+//stack(primitive)
+let myname="avinash"
+// let nickname=myname
+// console.log(nickname) //avinash
+// nickname="Lala"
+// console.log(myname) //avinash
+// console.log(nickname) //Lala    bcz had given only copy to nickname
+
+
+let user1={
+    name:"Avinash",
+    email:"avi86@gmail.com"
+}
+
+
+let user2=user1
+
+console.log(user1.name)
+user2.name="ashish"
+
+console.log(user1.name) //ashish -->user1 name hase baeen changed
+
+
+
+
+
+
+
+//heap(non primitive)
