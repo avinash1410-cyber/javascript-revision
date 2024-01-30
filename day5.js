ea2352d919a4f0b4b121f7671e9f9ed0877d232b
avinash.kumar1 <avinash.kumar1@spinny.com>
2024-01-30 21:36:05 +0530

    intro of function and other d    age

---
+++ day5.js
@@ -0,0 +1,46 @@
+//primitive data types
+/*
+ String
+ boolean
+ Number
+ null
+ undefined
+ BigInt
+ symbol
+*/
+
+
+
+//non primitive/refrence data types
+
+/**
+ * object
+ * Array
+ * Function
+ */
+
+// js is dynamically types languages as we not have to defined the datatype of varibale
+
+
+
+
+
+//Array
+const hero=["Iron man","shakti man","Hulk"]
+let details={
+    name:"Avinash",
+    age:24,
+    city:"delhi"
+}
+
+let myfn=function(){
+    console.log("Hello mr avinash kumar")
+}
+
+
+
+
+
+console.log(typeof myfn)
+
+
