30d96c88d9645a83b582b8deb74f127f3397d2df
avinash.kumar1 <avinash.kumar1@spinny.com>
2024-01-30 13:02:29 +0530

    day1

--- day1.js
+++ day1.js
@@ -1,2 +1,86 @@
+"use strict"; //treating the code on new js funda
+
+
+
 //day for learning the javascript
-console.log("first")
+// const accountId=123456
+// let email="avi86@gmail.com"
+// var naam="Avinash"
+// city="delhi"
+
+
+// accountId=12345      cant change const value 
+// email="avi86@yahoo.com"
+// naam="Avinash Kumar"
+// city="new delhi"
+
+
+
+// console.log(accountId)
+// console.log(email)
+// console.log(naam)
+// console.log(city)
+
+
+
+// console.log("first")
+
+
+// console.table([accountId,naam,city,email])//print in table form 
+
+
+
+
+/*
+new comment style 
+ var--> old Way--"has problem of scope"
+ let--> new way
+*/
+
+// let age;
+// console.log(age) //undefined
+
+
+/////////////////////////////    Lec-2  ///////////////////////////////////////////////////////////////////////////////////
+
+
+// alert("ALERT!!!!!!")      //not work bcz we are using and not the browser
+
+
+/*
+console.log(first);console.log(first)
+very bad readbility
+*/
+
+
+//datatypes
+// String
+// Number
+// Boolean
+// BigInt
+// null---> a value for empty
+// undefined--> value is not defined for value
+// Symbol--> for uniquness
+// object
+
+console.log(typeof(23))
+console.log(typeof("23"))
+console.log(typeof(null)) // give the op as object
+console.log(undefined) // give undefined as op
+console.log(typeof(false))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
