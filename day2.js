07c93747938fc50d6fbcea2f84165ec5c5bfb849
avinash.kumar1 <avinash.kumar1@spinny.com>
2024-01-30 13:39:13 +0530

    type conversion

---
+++ day2.js
@@ -0,0 +1,55 @@
+// let score="33"
+// let score2="A"
+// let score3=null
+// let score4=true
+// let score5=undefined
+
+
+
+
+// console.log(typeof(score))
+// console.log(typeof score2)
+// console.log(typeof score3)
+// console.log(typeof score4)
+// console.log(typeof score5)
+
+
+
+
+// let val_in_no=Number(score) // convert the no in string 
+// let val_in_no_2=Number(score2)
+// let val_in_no_3=Number(score3)
+// let val_in_no_4=Number(score4)
+// let val_in_no_5=Number(score5)
+
+
+// console.log(typeof(val_in_no))
+// console.log(typeof(val_in_no_2))
+// console.log(typeof val_in_no_3)
+// console.log(typeof val_in_no_4)
+// console.log(typeof val_in_no_5)
+
+
+
+
+
+// console.log(val_in_no)
+// console.log(val_in_no_2) // NaN as op
+// console.log(val_in_no_3)
+// console.log(val_in_no_4) // give 1 as op   bcz value was true
+// console.log(val_in_no_5) // give NaN -->>>> not a number
+
+
+// let isLogIn=1
+// let isLogIn_2=""
+// let bool_val=Boolean(isLogIn_2)
+// console.log(bool_val)
+
+
+let no=null
+
+let in_str=String(no)
+console.log(in_str)
+console.log(typeof in_str)
+
+
