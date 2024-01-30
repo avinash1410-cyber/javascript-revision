1f423f2b27b3ae1c5ee2dd961a6927edd358feb2
avinash.kumar1 <avinash.kumar1@spinny.com>
2024-01-30 13:55:32 +0530

    operations and postfix,prefix

---
+++ day3.js
@@ -0,0 +1,57 @@
+//operations
+
+// let v1=2
+// let v2=3
+// console.log(-v2)
+
+
+
+// by following the ecma script funda
+
+// console.log(1 +"2"+2)//122
+// console.log(1+2+"2")//32
+// console.log("1" +2+2)//122
+
+// console.log(+true) // op as 1
+// console.log(true+) // error
+
+// v1=false
+// console.log(+v1) //op as 0
+// console.log(v1)
+
+
+//postfix
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+// bcz // returns the value before incrementing.
+
+
+
+//prefix
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+// bcz returns the value after incrementing.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
