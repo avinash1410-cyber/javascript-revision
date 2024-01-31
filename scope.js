91c4c48523046ac67e6de935234db2cd98b882f1
avinash.kumar1 <avinash.kumar1@spinny.com>
2024-01-31 21:52:34 +0530

    fucntion

--- scope.js
+++ scope.js
@@ -1,12 +1,71 @@
-a=10
+// a=10
 
-if(true){
-    let a =1;
-    var b=2;
-    const c=3;
-    console.log(a)
-}
+// if(true){
+//     let a =1;
+//     var b=2;
+//     const c=3;
+//     console.log(a)
+// }
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+
+
+
+// function one()
+// {
+
+//     username="Avinash"
+//     function two(){
+//         website="Google"
+//         console.log(username)
+//     }
+//     two()
+//     console.log(website)
+    
+// }
+// one()
+
+
+
+// if(true){
+//     username="Ak"
+//     if(username==="Ak"){
+//         console.log(username)
+//     }
+// }
+
+
+
+
+
+
+// console.log(addone(5))
+// function addone(num){
+//     return num+1
+// }
+
+// console.log(addone(5))
+
+
+
+// console.log(addtwo(5));
+// const addtwo = function(num) {
+//     return num + 2;
+// }
+//abobe one is giving Error
+
+
+
+
+
+
+
+
+
+
+
+
+
+
