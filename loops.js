a878cd86356a27c2f64a51e650d69fd91b0779a1
avinash.kumar1 <avinash.kumar1@spinny.com>
2024-02-01 15:31:52 +0530

    for loops

---
+++ loops.js
@@ -0,0 +1,38 @@
+ //for
+//  arr=[3,2,8,7,6,7,3,2]
+//  for (let i=0;i<arr.length;i++){
+//     console.log(arr[i])
+//  }
+
+
+// for(let i=0;i<arr.length;i++){
+//     console.log("Outer-----------------")
+//     for(let j=0;j<arr.length;j++)
+//     {
+//         console.log("Inner")
+//     }
+// }
+
+
+
+
+for(i=0;i<20;i++){
+    if(i==5){
+        console.log("We got 5")
+        break
+    }
+    console.log(i)
+}
+
+
+
+for(i=0;i<20;i++){
+    if(i==5){
+        console.log("We got 5")
+        continue
+    }
+    console.log(i)
+}
+
+
+
