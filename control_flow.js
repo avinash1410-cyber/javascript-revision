94dac517f4db7307bbf141291e4862f757aefed8
avinash.kumar1 <avinash.kumar1@spinny.com>
2024-02-01 14:52:42 +0530

    if-else,truthy,nullish,ternary operator

---
+++ control_flow.js
@@ -0,0 +1,91 @@
+//if
+
+
+// if(2!=3){
+//     console.log("true")
+// }
+// else{
+//     console.log("False")
+// }
+
+
+// let bal=400
+// if (bal>300) console.log("mpre than 300");
+// console.log(bal)
+
+
+
+// a=false
+// b=false
+
+
+// if(a || b){
+// console.log("one is true")
+// }
+// else{
+// console.log("both are fasle")
+// }
+
+// let month="Aprail"
+
+// switch(month){
+//     case 1:
+//         console.log("january")
+//         break;
+//     case 2:
+//         console.log("february")
+//         break;
+//     case 3:
+//         console.log("March")
+//         break;
+//     default:
+//         console.log("Not anyone ")
+// }
+
+
+//falsy value
+// false,0,-0,BigInt 0n,"",null,undefined,NaN
+//truthy vale
+// [],true,"0","false"," ",{},function(){}
+
+// let arr=[]
+
+// if (arr.length===0){
+//     console.log("Empty Array")
+// }
+//  let obj={}
+//  if(Object.keys(obj).length===0){
+//     console.log("Empty Object")
+//  }
+
+
+// Nullish coaclecasing operator(??) : null undefined
+
+// let v1;
+// v1=5??10
+// v1=null??10
+// console.log(v1)
+
+// v1=undefined??14
+// console.log(v1)
+
+// v1=null ??undefined??10??20
+
+// console.log(v1)
+
+
+
+//Terniary operator
+// condition ?true:false
+
+let ice=1
+ice>10?console.log("costly"):console.log("cheap")
+
+
+
+
+
+
+
+
+
